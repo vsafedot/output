9eb67e96dadb72fa13b811e43a6bc6c8bf1136b8
vsafedot <siddharthnain3@gmail.com>
2025-01-21 14:02:33 +0530

    modified:   second.java

--- MatrixOperations.java
+++ MatrixOperations.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class second {
+public class MatrixOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
